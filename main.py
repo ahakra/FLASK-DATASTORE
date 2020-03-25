395241d5b4b6468910f8521f57acc036bbd8ecfb
ahakra <ah.akra@gmail.com>
2020-03-25 22:07:03 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+from google.cloud import datastore
+
+import logging
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+     return render_template("home.html",author = " ")
+
+
+@app.route("/discuss",methods=['GET'])
+def discuss():
+     return render_template("discuss.html")
+
+@app.route("/submit")
+def submit():
+     name = request.args.get("name")
+     email = request.args.get("email")
+     topic = request.args.get("topic")
+     message = request.args.get("message")
+     if ( len(name) == 0 and len(email) == 0  and len(topic) == 0 ):
+            app.logger.info("message is epty")
+
+     else:     
+           datastore_client = datastore.Client("vmwordpresssql")
+           key = datastore_client.key('Topic')
+           q_entity = datastore.Entity(key=key)
+           q_entity["name"]=name
+           q_entity["email"]=email
+           q_entity["topic"]=topic
+           q_entity["topicDescription"]=message
+           datastore_client.put(q_entity)
+           app.logger.info("name: " + name+" email: " + email + " topic: " + topic)
+     return render_template("home.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
